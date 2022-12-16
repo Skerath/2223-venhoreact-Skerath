8602eca04a82309d4f99e978db090ef0c572d176
Skerath <cantaert.maurice+git@gmail.com>
2022-12-17 00:47:10 +0100

    Filter: * Split code into mapFilterGroups() and mapFilters() to make code more readable

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -7,31 +7,39 @@ export default function Filter({input, output}) {
     // TODO: properly use ref
     //  "Unexpected ref object provided for select. Use either a ref-setter function or React.createRef()."
 
-    const onUserInput = () => {
+    function onUserInput() {
         let outputs = [];
         input.forEach(sublist => {
             sublist.filterObjects.forEach(filterObject => outputs.push(filterObject.ref.current.value))
         });
         output(outputs);
-    };
+    }
 
     return (
         <div className="d-flex flex-row flex-wrap justify-content-center mb-3">
-            {input.map((sublist, sublistcounter) => { // filter groups
-                return (
-                    <div className="input-group" style={sublist.stylingOptions} key={sublistcounter}>
-                        {sublist.filterObjects.map((item, itemcounter) => { // individual filter objects
-                            return (
-                                item.inputType ?
-                                    <FilterInput inputObject={item} onChange={onUserInput}
-                                                 key={`${sublistcounter}-${itemcounter}`}></FilterInput> :
-                                    <FilterSelect selectObject={item} onChange={onUserInput}
-                                                  key={`${sublistcounter}-${itemcounter}`}></FilterSelect>
-                            );
-                        })}
-                    </div>
-                )
-            })}
+            {mapFilterGroups(input, onUserInput)}
         </div>
     );
 };
+
+function mapFilterGroups(input, onUserInput) {
+    return input.map((sublist, sublistcounter) => {
+        return (
+            <div className="input-group" style={sublist.stylingOptions} key={sublistcounter}>
+                {mapFilters(sublist, sublistcounter, onUserInput)}
+            </div>
+        );
+    });
+}
+
+function mapFilters(sublist, sublistcounter, onUserInput) {
+    return sublist.filterObjects.map((item, itemcounter) => {
+        return (
+            item.inputType ?
+                <FilterInput inputObject={item} onChange={onUserInput}
+                             key={`${sublistcounter}-${itemcounter}`}></FilterInput> :
+                <FilterSelect selectObject={item} onChange={onUserInput}
+                              key={`${sublistcounter}-${itemcounter}`}></FilterSelect>
+        );
+    });
+}
