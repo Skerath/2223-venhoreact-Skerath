1cb9712dc638ef06cd4fad05cf6b1bf0163e7c50
Skerath <cantaert.maurice+git@gmail.com>
2022-12-18 22:51:51 +0100

    Project * Added ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,90 @@
 import {Route, Routes} from "react-router";
 import {Home, IngredientsPage, Locations} from "./pages";
 import Navbar from "./components/navbar/Navbar";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
     return (
-        <Routes>
-            <Route index element={
-                <>
-                    <Navbar/>
-                    <Home/>
-                </>
-            }/>
-
-            <Route path="items">
+        <>
+            <ScrollToTop/>
+            <Routes>
                 <Route index element={
                     <>
                         <Navbar/>
-                        <p>Not implemented yet.</p>
+                        <Home/>
                     </>
                 }/>
 
-            </Route>
-            {/*TODO implement*/}
-            <Route path="locations" element={
-                <>
-                    <Navbar/>
-                    <Locations/>
-                </>
-            }/>
-            <Route path="resources">
-                <Route path="ingredients" element={
-                    <>
-                        <Navbar/>
-                        <IngredientsPage/>
-                    </>
-                }/>
+                <Route path="items">
+                    <Route index element={
+                        <>
+                            <Navbar/>
+                            <p>Not implemented yet.</p>
+                        </>
+                    }/>
+
+                </Route>
                 {/*TODO implement*/}
-                <Route path="resources" element={
+                <Route path="locations" element={
                     <>
                         <Navbar/>
-                        <p>Not implemented yet.</p>
+                        <Locations/>
                     </>
                 }/>
-                {/*TODO implement*/}
-                <Route path="powders" element={
+                <Route path="resources">
+                    <Route path="ingredients" element={
+                        <>
+                            <Navbar/>
+                            <IngredientsPage/>
+                        </>
+                    }/>
+                    {/*TODO implement*/}
+                    <Route path="resources" element={
+                        <>
+                            <Navbar/>
+                            <p>Not implemented yet.</p>
+                        </>
+                    }/>
+                    {/*TODO implement*/}
+                    <Route path="powders" element={
+                        <>
+                            <Navbar/>
+                            <p>Not implemented yet.</p>
+                        </>
+                    }/>
+                    {/*TODO implement*/}
+                    <Route path="potions" element={
+                        <>
+                            <Navbar/>
+                            <p>Not implemented yet.</p>
+                        </>
+                    }/>
+                </Route>
+                <Route path="items">
+                    {/*TODO implement*/}
+                    <Route path="items" element={
+                        <>
+                            <Navbar/>
+                            <p>Not implemented yet.</p>
+                        </>
+                    }/>
+                    {/*TODO implement*/}
+                    <Route path="custom-items" element={
+                        <>
+                            <Navbar/>
+                            <p>Not implemented yet.</p>
+                        </>
+                    }/>
+                </Route>
+                {/*TODO proper page not found page*/}
+                <Route path="*" element={
                     <>
                         <Navbar/>
-                        <p>Not implemented yet.</p>
+                        <p>Page not found.</p>
                     </>
                 }/>
-                {/*TODO implement*/}
-                <Route path="potions" element={
-                    <>
-                        <Navbar/>
-                        <p>Not implemented yet.</p>
-                    </>
-                }/>
-            </Route>
-            <Route path="items">
-                {/*TODO implement*/}
-                <Route path="items" element={
-                    <>
-                        <Navbar/>
-                        <p>Not implemented yet.</p>
-                    </>
-                }/>
-                {/*TODO implement*/}
-                <Route path="custom-items" element={
-                    <>
-                        <Navbar/>
-                        <p>Not implemented yet.</p>
-                    </>
-                }/>
-            </Route>
-            {/*TODO proper page not found page*/}
-            <Route path="*" element={
-                <>
-                    <Navbar/>
-                    <p>Page not found.</p>
-                </>
-            }/>
-        </Routes>
+            </Routes>
+        </>
     );
 }
 
