ed51a1156afc46acbe5040b1809bcf54551e0a6a
Skerath <cantaert.maurice+git@gmail.com>
2022-12-22 03:53:18 +0100

    Project: * added Formik and Yup for form validation
    Create Item:
    * form now gets validated

--- src/pages.jsx
+++ src/pages.jsx
@@ -4,8 +4,10 @@ import {INGREDIENTS_FILTER_LAYOUT, ITEM_FILTER_LAYOUT} from "./components/filter
 import Ingredients from "./components/ingredients/Ingredients";
 import {Home} from "./components/home/Home";
 import Items from "./components/items/Items";
-import {Button, Card, Container, Form} from "react-bootstrap";
+import {Button, Card, Form} from "react-bootstrap";
 import './NewItemPage.css'
+import {Formik} from "formik";
+import * as yup from "yup";
 
 const queryPrefix = [];
 
@@ -45,54 +47,118 @@ export const ItemsPage = () => {
     );
 };
 
+const validationSchema = yup.object().shape({
+    name: yup.string()
+        .required("Item name is required")
+        .min(3, "Item name must have at least 3 characters")
+        .max(60, "Item name can't be longer than 60 characters"),
+    type: yup.string()
+        .required("Type must be selected"),
+    ingredient: yup.string()
+        .required("Ingredient name is required"),
+});
+
 export const NewItemPage = () => {
+
     return (
         <main style={{display: "inline-flex", justifyContent: "center", width: "100vw"}}>
             <Card bg="dark" style={{width: "50rem"}}>
                 <Card.Header className={"h2"}>Submit new item
                 </Card.Header>
                 <Card.Body>
-                    <Form style={{width: "90%"}}>
-                        <Form.Group className="mb-3" controlId="formName">
-                            <Form.Label>Item Name</Form.Label>
-                            <Form.Control className="bg-dark" type="text" placeholder="Ex.: Combat - Ragni Region"/>
-                            <Form.Text className="text-muted">
-                                Try to follow the guild's naming scheme: "Usecase - Area"
-                            </Form.Text>
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formName">
-                            <Form.Label>Item Type</Form.Label>
-                            <Form.Select className="bg-dark" type="text">
-                                <option value="HELMET">Helmet</option>
-                                <option value="CHESTPLATE">Chestplate</option>
-                                <option value="LEGGINGS">Leggings</option>
-                                <option value="BOOTS">Boots</option>
-                                <option value="SPEAR">Spear</option>
-                                <option value="DAGGER">Dagger</option>
-                                <option value="BOW">Bow</option>
-                                <option value="WAND">Wand</option>
-                                <option value="RELIK">Relik</option>
-                                <option value="RING">Ring</option>
-                                <option value="BRACELET">Bracelet</option>
-                                <option value="NECKLACE">Necklace</option>
-                                <option value="POTION">Potion</option>
-                                <option value="SCROLL">Scroll</option>
-                                <option value="FOOD">Food</option>
-                            </Form.Select>
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formName">
-                            <Form.Label>Ingredient Used</Form.Label>
-                            <Form.Control className="bg-dark" type="text" placeholder="Ex.: Accursed Effigy"/>
-                            <Form.Text className="text-muted">
-                                Make sure the ingredient is spelled correctly and is usable for the selected item type
-                            </Form.Text>
-                        </Form.Group>
-                        <Button variant="outline-light" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
+                    <Formik
+                        validationSchema={validationSchema}
+                        onSubmit={(values, {setSubmitting, resetForm}) => {
+                            setSubmitting(true)
+                            resetForm();
+                            setSubmitting(false);
+                        }}
+                        initialValues={{
+                            name: '',
+                            type: '',
+                            ingredient: '',
+                        }}
+                    >
+                        {({
+                              handleSubmit,
+                              handleChange,
+                              handleBlur,
+                              values,
+                              touched,
+                              isSubmitting,
+                              errors,
+                          }) => (
+                            <Form noValidate onSubmit={handleSubmit} style={{width: "90%"}}>
+                                <Form.Group className="mb-3" controlId="validationFormik01">
+                                    <Form.Label>Item Name</Form.Label>
+                                    <Form.Control type="text"
+                                                  className={`bg-dark ${touched.name && errors.name ? "is-invalid" : null}`}
+                                                  placeholder="Ex.: Combat - Ragni Region"
+                                                  name="name"
+                                                  value={values.name}
+                                                  onChange={handleChange}
+                                                  onBlur={handleBlur}
+                                                  isValid={touched.name && !errors.name}/>
+                                    <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
+                                    <Form.Text className="text-muted">
+                                        Try to follow the guild's naming scheme: "Usecase - Area"
+                                    </Form.Text>
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formType">
+                                    <Form.Label>Item Type</Form.Label>
+                                    <Form.Select type="text"
+                                                 className={`bg-dark ${touched.name && errors.type ? "is-invalid" : null}`}
+                                                 name="type"
+                                                 value={values.type}
+                                                 onChange={handleChange}
+                                                 onBlur={handleBlur}
+                                                 isValid={touched.type && !errors.type}>
+                                        <option></option>
+                                        <option value="HELMET">Helmet</option>
+                                        <option value="CHESTPLATE">Chestplate</option>
+                                        <option value="LEGGINGS">Leggings</option>
+                                        <option value="BOOTS">Boots</option>
+                                        <option value="SPEAR">Spear</option>
+                                        <option value="DAGGER">Dagger</option>
+                                        <option value="BOW">Bow</option>
+                                        <option value="WAND">Wand</option>
+                                        <option value="RELIK">Relik</option>
+                                        <option value="RING">Ring</option>
+                                        <option value="BRACELET">Bracelet</option>
+                                        <option value="NECKLACE">Necklace</option>
+                                        <option value="POTION">Potion</option>
+                                        <option value="SCROLL">Scroll</option>
+                                        <option value="FOOD">Food</option>
+                                    </Form.Select>
+                                    <Form.Control.Feedback type="invalid">{errors.type}</Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formIngredient">
+                                    <Form.Label>Ingredient Used</Form.Label>
+                                    <Form.Control type="text"
+                                                  className={`bg-dark ${touched.name && errors.name ? "error" : null}`}
+                                                  placeholder="Ex.: Accursed Effigy"
+                                                  name="ingredient"
+                                                  value={values.ingredient}
+                                                  onChange={handleChange}
+                                                  onBlur={handleBlur}
+                                                  isValid={touched.ingredient && !errors.ingredient}/>
+                                    <Form.Control.Feedback type="invalid">{errors.ingredient}</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">This form being green does not mean the
+                                        Ingredient is found! Please double check.</Form.Control.Feedback>
+                                    <Form.Text className="text-muted">
+                                        Make sure the ingredient is spelled correctly and is usable for the selected
+                                        item type
+                                    </Form.Text>
+                                </Form.Group>
+                                <Button variant="outline-light" type="submit" disabled={isSubmitting}>
+                                    Submit Item
+                                </Button>
+                            </Form>
+                        )}
+                    </Formik>
                 </Card.Body>
             </Card>
         </main>
-    );
+    )
+        ;
 };
