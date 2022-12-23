62afc6dea01f92a7fc04503da47a1adaf6c57f4b
Skerath <cantaert.maurice+git@gmail.com>
2022-12-23 23:15:26 +0100

    Project: * cleanup

--- src/components/alert/Error.jsx
+++ src/components/alert/Error.jsx
@@ -10,7 +10,6 @@ export const Error = ({styling, error}) => {
                     <h3 className="alert-heading"><strong>An error occured:&nbsp;</strong></h3>
                     <h4 className="mb-3">{error.message || JSON.stringify(error)}</h4>
                     <hr/>
-                    <h6>Check the status of the app <a href="#" className="alert-link">here</a>.</h6>
                     <h6>If this error persists, please contact me <a href="https://discord.com/users/874720684524339343"
                                                                      className="alert-link">on Discord</a>.</h6>
                 </div>
